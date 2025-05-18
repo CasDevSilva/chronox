c7188e60690bec5e765129b2a3af00430a775cd9
CasDevSilva <sca.dev.silva@gmail.com>
2025-05-18 18:56:30 -0500

    dev: Report command single and plural

---
+++ src/commands/report.js
@@ -0,0 +1,80 @@
+import * as Database from "../../utils/database.js";
+import Table from "cli-table3";
+
+export function reportProjects() {
+    try {
+        const db = Database.connectDatabase();
+        let mArrTtlSessions = null;
+
+        if (
+            ([...arguments].length &&
+            (Array.isArray(arguments[0]) && arguments[0].length == 0)) ||
+            [...arguments].length == 0
+        ) {
+            mArrTtlSessions = db.prepare(`
+                SELECT
+                    projects.alias,
+                    projects.name,
+                    MIN(sessions.date_start) date_start,
+                    MAX(sessions.date_end) date_end,
+                    SUM(sessions.duration) duration
+                FROM projects
+                JOIN sessions
+                    ON projects.id = sessions.project_id
+                GROUP BY 1, 2
+                ORDER BY 5 DESC
+            `).all();
+        } else {
+            if ([...arguments].length == 1) {
+                mArrTtlSessions = [db.prepare(`
+                    SELECT
+                        projects.alias,
+                        projects.name,
+                        MIN(sessions.date_start) date_start,
+                        MAX(sessions.date_end) date_end,
+                        SUM(sessions.duration) duration
+                    FROM projects
+                    JOIN sessions
+                        ON projects.id = sessions.project_id
+                    WHERE
+                        projects.alias = ?
+                    GROUP BY 1, 2
+                    ORDER BY 5 DESC
+                `).get(...arguments[0])];
+            } else {
+                console.log("Error only requests the total of a project, not more.");
+            }
+        }
+
+        if (Array.isArray(mArrTtlSessions)) {
+            let mRsTable = new Table({
+                head: ["Alias", "Name of Project", "Date Start", "Date End", "Duration"],
+                colWidths: [30, 50, 30, 30, 30],
+                style: {
+                    head: ["cyan"],
+                    border: ["grey"]
+                }
+            })
+
+            mArrTtlSessions.forEach(mRowSession => {
+                mRsTable.push([
+                    mRowSession.alias,
+                    mRowSession.name,
+                    mRowSession.date_start,
+                    mRowSession.date_end,
+                    mRowSession.duration
+                    ]
+                )
+            })
+
+            console.log(mRsTable.toString());
+        } else {
+            console.log("Error when generating the time average");
+        }
+
+        db.close();
+
+    } catch (err) {
+        console.log("Error while compiling the time of the project(s)");
+    }
+}
