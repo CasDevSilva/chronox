a3b010284600d4123e9667d9d76b63441bcdba0b
CasDevSilva <sca.dev.silva@gmail.com>
2025-05-24 13:02:27 -0500

    dev: Database documentation

--- utils/database.js
+++ utils/database.js
@@ -1,17 +1,32 @@
 import pkg from "better-sqlite3";
+import chalk from "chalk";
 
 const Database = pkg;
 
+/**
+ * Name: connectDatabase
+ *
+ * Description: This function is used to connect to the database.
+ *
+ * Return: It returns the database connection.
+*/
 export function connectDatabase(){
-    let db = new Database("./db/chronox.db")
-
+    let db = new Database("./db/chronox.db");
     return db;
 }
 
+/**
+ * Name: createTable
+ *
+ * Description: This function is used to create a table in the database.
+ *
+ * @params {String} pStrTableName - The name of the table to be created.
+ * @params {Object} pObjTableColumns - The columns of the table to be created.
+*/
 function createTable(pStrTableName, pObjTableColumns) {
-    let mArrColumns = []
+    let mArrColumns = [];
     Object.keys(pObjTableColumns).forEach(mRowColumn => {
-        mArrColumns.push(`${mRowColumn} ${pObjTableColumns[mRowColumn]}`)
+        mArrColumns.push(`${mRowColumn} ${pObjTableColumns[mRowColumn]}`);
     })
 
     try {
@@ -21,16 +36,20 @@ function createTable(pStrTableName, pObjTableColumns) {
             CREATE TABLE IF NOT EXISTS ${pStrTableName} (
                 ${mArrColumns.join(', ')}
             )
-        `
+        `;
 
-        db.exec(mStrSQLSentence)
-
+        db.exec(mStrSQLSentence);
         db.close();
     } catch(err) {
-        throw new Error("ERROR to Create the Tables")
+        throw new Error("ERROR to Create the Tables");
     }
 }
 
+/**
+ * Name: syncDatabase
+ *
+ * Description: This function is used to synchronize the database.
+*/
 export function syncDatabase() {
     let mObjTables = {
         projects: {
@@ -51,7 +70,6 @@ export function syncDatabase() {
         }
     }
 
-    Object.keys(mObjTables).forEach(mRowTable => createTable(mRowTable, mObjTables[mRowTable]))
-
-    console.log("Init the Module")
+    Object.keys(mObjTables).forEach(mRowTable => createTable(mRowTable, mObjTables[mRowTable]));
+    console.log(chalk.green("Init the Module"));
 }
