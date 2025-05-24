3dd382b67ab38255a828281e0a9c551c95bc48da
CasDevSilva <sca.dev.silva@gmail.com>
2025-05-24 13:05:18 -0500

    dev: Export command and documentation

--- src/commands/export.js
+++ src/commands/export.js
@@ -4,16 +4,44 @@ import ExcelJS from 'exceljs';
 import dayjs from "dayjs";
 import fs from "fs";
 import * as Database from "../../utils/database.js";
+import chalk from "chalk";
 
 const baseUrl = path.join(os.homedir(), ".chronox", "exports");
 
-export async function exportCsv() {
+/**
+ * Name: exportCsv
+ *
+ * Description: This function exports the projects and sessions to a CSV file.
+ *
+ * @param {Array} pArrParams - The parameters passed to the function.
+*/
+export async function exportCsv(pArrParams) {
     if (!fs.existsSync(baseUrl)) {
         fs.mkdirSync(baseUrl, { recursive: true });
     }
 
+    /**
+     * Check if the user passed more than one parameter
+     * If so, return an error message
+    */
+    if (pArrParams.length > 1) {
+        console.log(chalk.red("Error: Only one parameter is allowed, the project alias. Not more."));
+        return;
+    }
+
     try {
+        /**
+         * Connect to the database
+         * Create a new workbook, create a sheets for projects and sessions
+        */
         const db = Database.connectDatabase();
+
+        let mObjProject = db.prepare(`SELECT * FROM projects WHERE alias = ?`).get(pArrParams[0]);
+        if (!mObjProject) {
+            console.log(chalk.yellow(`Error: Project with alias "${pArrParams[0]}" not found.`));
+            return;
+        }
+
         const workbook = new ExcelJS.Workbook();
 
         const mProjectsSheet = workbook.addWorksheet('Projects');
@@ -35,50 +63,47 @@ export async function exportCsv() {
                 { header: 'Duration'    , key: 'duration' }
             ];
 
-        if (
-            ([...arguments].length &&
-            (Array.isArray(arguments[0]) && arguments[0].length == 0)) ||
-            [...arguments].length == 0
-        ) {
-            let mRsProjects = db.prepare(`SELECT * FROM projects`).all()
-            let mRsSessions = db.prepare(`SELECT * FROM sessions ORDER BY project_id`).all();
+        let mRsProjects = null;
+        let mRsSessions = null;
+        mObjProject = null;
 
-            if(Array.isArray(mRsProjects)){mProjectsSheet.addRows(mRsProjects)}
-            if(Array.isArray(mRsSessions)){mSessionsSheet.addRows(mRsSessions)}
-
-            let mStrFullPath = path.join(baseUrl, `report_chronox_${dayjs().format("DD_MM_YYYY(HH_mm_ss)")}.xlsx`)
-            let mStrMidPath = path.join("~", ".chronox", "exports",`report_chronox_${dayjs().format("DD_MM_YYYY(HH_mm_ss)")}.xlsx`)
-
-            await workbook.xlsx.writeFile(mStrFullPath);
-
-            console.log(`File Created in: "${mStrMidPath}"`)
+        /**
+         * Check if the user set a project alias to filters the projects and sessions.
+         * Then, add information to the sheets.
+         * Finally, write the file to the disk.
+         *
+         * Inform the user the path where the file was created.
+        */
+        if (pArrParams.length == 0) {
+            mRsProjects = db.prepare(`SELECT * FROM projects`).all()
+            mRsSessions = db.prepare(`SELECT * FROM sessions ORDER BY project_id`).all();
         } else {
-            if ([...arguments].length == 1) {
-                let mObjProject = db.prepare(`SELECT * FROM projects WHERE alias = ?`).get(...arguments[0])
-                let mRsSessions = db.prepare(`
-                    SELECT sessions.*
-                    FROM sessions
-                        JOIN projects ON projects.id = sessions.project_id
-                    WHERE
-                        alias = ?
-                `).all(...arguments[0]);
-
-                if (mObjProject) {mProjectsSheet.addRow(mObjProject)}
-                if (Array.isArray(mRsSessions)) {mSessionsSheet.addRows(mRsSessions)}
-
-                let mStrFullPath = path.join(baseUrl, `report_chronox_${mObjProject.alias}_${dayjs().format("DD_MM_YYYY(HH_mm_ss)")}.xlsx`)
-                let mStrMidPath = path.join("~", ".chronox", "exports",`report_chronox_${mObjProject.alias}_${dayjs().format("DD_MM_YYYY(HH_mm_ss)")}.xlsx`);
-
-                await workbook.xlsx.writeFile(mStrFullPath);
-
-                console.log(`File Created in: "${mStrMidPath}"`)
-            } else {
-                console.log("Error only requests the total of a project, not more.");
-            }
+            mObjProject = db.prepare(`SELECT * FROM projects WHERE alias = ?`).get(...arguments[0])
+            mRsSessions = db.prepare(`
+                SELECT sessions.*
+                FROM sessions
+                    JOIN projects ON projects.id = sessions.project_id
+                WHERE
+                    alias = ?
+            `).all(...arguments[0]);
         }
+
+        if (Array.isArray(mRsProjects)) {mProjectsSheet.addRows(mRsProjects)}
+        if (Array.isArray(mRsSessions)) {mSessionsSheet.addRows(mRsSessions)}
+        if (mObjProject) {mProjectsSheet.addRow(mObjProject)}
+
+        let mStrFullPath = pArrParams.length == 0
+            ? path.join(baseUrl, `report_chronox_${dayjs().format("DD_MM_YYYY(HH_mm_ss)")}.xlsx`)
+            : path.join(baseUrl, `report_chronox_${mObjProject.alias}_${dayjs().format("DD_MM_YYYY(HH_mm_ss)")}.xlsx`);
+        let mStrMidPath = pArrParams.length == 0
+            ? path.join("~", ".chronox", "exports",`report_chronox_${dayjs().format("DD_MM_YYYY(HH_mm_ss)")}.xlsx`)
+            : path.join("~", ".chronox", "exports",`report_chronox_${mObjProject.alias}_${dayjs().format("DD_MM_YYYY(HH_mm_ss)")}.xlsx`);
+
+        await workbook.xlsx.writeFile(mStrFullPath);
+        console.log(chalk.green(`File Created in: "${mStrMidPath}"`))
 
         db.close();
     } catch(err) {
-        console.log(`Error generating Csv file`)
+        console.log(chalk.red(`Error generating Csv file`))
     }
 }
