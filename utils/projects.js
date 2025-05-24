e4b141bbdbe9e1f4ee93a7ab099e3df71160cd18
CasDevSilva <sca.dev.silva@gmail.com>
2025-05-24 15:13:49 -0500

    dev: Table lenght to List

--- utils/projects.js
+++ utils/projects.js
@@ -59,7 +59,7 @@ export function listProjects(pArrParams) {
     let mRsTable = new Table(
         {
             head: ["Alias code", "Name of Project", "Status", "Date created", "Date end"],
-            colWidths: [30, 50, 30, 30, 30],
+            colWidths: [20, 40, 10, 20, 20],
             compact: true,
             style: {
                 head: ["cyan"],
