b2d83d06560c699fe8a9e110454f6e1117d7044a
CasDevSilva <sca.dev.silva@gmail.com>
2025-05-24 13:05:28 -0500

    dev: Report  command and documentation

--- src/commands/report.js
+++ src/commands/report.js
@@ -1,16 +1,31 @@
 import * as Database from "../../utils/database.js";
 import Table from "cli-table3";
+import chalk from "chalk";
 
-export function reportProjects() {
+/**
+ * Name: reportProjects
+ *
+ * Description: This function is used to report the time of a project or all projects.
+ *
+ * @params {Array} pArrParams - Array of parameters passed to the function.
+*/
+export function reportProjects(pArrParams) {
+    /**
+     * Check if the user has passed only one param(project alias), not more.
+    */
+    if (pArrParams.length > 1) {
+        console.log(chalk.red("Only refer an alias a project, not more"));
+        return;
+    }
+
     try {
         const db = Database.connectDatabase();
         let mArrTtlSessions = null;
 
-        if (
-            ([...arguments].length &&
-            (Array.isArray(arguments[0]) && arguments[0].length == 0)) ||
-            [...arguments].length == 0
-        ) {
+        /**
+         * Based on the number of params, get the sessions of the project or all projects.
+        */
+        if (pArrParams.length == 0) {
             mArrTtlSessions = db.prepare(`
                 SELECT
                     projects.alias,
@@ -26,32 +41,32 @@ export function reportProjects() {
                 ORDER BY 5 DESC
             `).all();
         } else {
-            if ([...arguments].length == 1) {
-                mArrTtlSessions = [db.prepare(`
-                    SELECT
-                        projects.alias,
-                        projects.name,
-                        projects.rate,
-                        MIN(sessions.date_start) date_start,
-                        MAX(sessions.date_end) date_end,
-                        SUM(sessions.duration) duration
-                    FROM projects
-                    JOIN sessions
-                        ON projects.id = sessions.project_id
-                    WHERE
-                        projects.alias = ?
-                    GROUP BY 1, 2
-                    ORDER BY 5 DESC
-                `).get(...arguments[0])];
-            } else {
-                console.log("Error only requests the total of a project, not more.");
-            }
+            mArrTtlSessions = [db.prepare(`
+                SELECT
+                    projects.alias,
+                    projects.name,
+                    projects.rate,
+                    MIN(sessions.date_start) date_start,
+                    MAX(sessions.date_end) date_end,
+                    SUM(sessions.duration) duration
+                FROM projects
+                JOIN sessions
+                    ON projects.id = sessions.project_id
+                WHERE
+                    projects.alias = ?
+                GROUP BY 1, 2
+                ORDER BY 5 DESC
+            `).get(...arguments[0])];
         }
 
+        /**
+         * Validate if the query returned a result
+         * If not, show a message
+        */
         if (Array.isArray(mArrTtlSessions)) {
             let mRsTable = new Table({
                 head: ["Alias", "Name of Project", "Rate", "Date Start", "Date End", "Duration", "Cost"],
-                colWidths: [30, 50, 30, 30, 30, 30, 30],
+                colWidths: [20, 40, 20, 30, 30, 20, 20],
                 style: {
                     head: ["cyan"],
                     border: ["grey"]
@@ -60,25 +75,24 @@ export function reportProjects() {
 
             mArrTtlSessions.forEach(mRowSession => {
                 mRsTable.push([
-                    mRowSession.alias,
+                    chalk.bold(mRowSession.alias),
                     mRowSession.name,
-                    mRowSession.rate,
+                    chalk.blue(mRowSession.rate),
                     mRowSession.date_start,
                     mRowSession.date_end,
                     mRowSession.duration,
-                    (mRowSession.duration/3600 * mRowSession.rate).toFixed(2)
+                    chalk.greenBright((mRowSession.duration/3600 * mRowSession.rate).toFixed(2))
                     ]
                 )
             })
 
             console.log(mRsTable.toString());
         } else {
-            console.log("Error when generating the time average");
+            console.log(chalk.red("Error when generating the time average"));
         }
 
         db.close();
-
     } catch (err) {
-        console.log("Error while compiling the time of the project(s)");
+        console.log(chalk.red("Error while compiling the time of the project(s)"));
     }
 }
